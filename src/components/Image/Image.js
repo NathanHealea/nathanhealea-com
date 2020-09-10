55fcf6723ed9f74a64f8212b56a50de3a9a78c0a
Nathan Healea <nate.healea@gmail.com>
2020-09-09 23:11:27 -0700

    Implement transition for image coloring and boarder

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,65 +1,64 @@
 import React from 'react';
 import color from 'tinycolor2';
 
-import { styled } from '@material-ui/core/styles';
+import { styled, withStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
 
-/**
- * TODO: Figure out how to implement animation with styled from MUI
- */
-const Image = styled('div')(({ theme, src, height, width }) => ({
-  position: 'relative',
-  height,
-  width,
-  marginRight: '32px',
-  marginBottom: '32px',
-  backgroundImage: `url(${src})`,
-  backgroundSize: 'cover',
-  backgroundColor: color(theme.palette.primary.main)
-    .setAlpha(50)
-    .toRgbString(),
-  backgroundBlendMode: 'luminosity, normal',
-  borderRadius: '8px',
-  transformStyle: 'preserve-3d',
-  '&:hover': {
-    backgroundColor: 'transparent',
-    backgroundBlendMode: 'normal',
-    '& > div': {
-      top: 16,
-      left: 16,
-    },
-  },
-  '& > div': {
+const ImageBox = withStyles((theme) => ({
+  root: (props) => ({
     position: 'relative',
-    width: '100%',
-    paddingTop: `calc(9/16 *100%)`,
+    height: props.height,
+    width: props.width,
+    marginRight: '32px',
+    marginBottom: '32px',
     borderRadius: '8px',
-    border: `2px solid ${theme.palette.primary.main}`,
-    transform: 'translateZ(-16px)',
-  },
-}));
+    backgroundSize: 'cover',
+    backgroundImage: `url(${props.src})`,
+    backgroundColor: color(theme.palette.primary.main)
+      .setAlpha(50)
+      .toRgbString(),
+    backgroundBlendMode: 'luminosity, normal',
+    transformStyle: 'preserve-3d',
+    transition: theme.transitions.create('All', {
+      easing: theme.transitions.easing.easeOut,
+      duration: 500,
+    }),
+    '&:hover': {
+      backgroundColor: 'transparent',
+      backgroundBlendMode: 'normal',
+      '& > div': {
+        top: 16,
+        left: 16,
+      },
+    },
+    '& > div': {
+      position: 'relative',
+      top: 32,
+      left: 32,
+      height: '100%',
+      width: '100%',
+      borderRadius: '8px',
+      border: `2px solid ${theme.palette.primary.main}`,
+      transform: 'translateZ(-16px)',
+      transition: theme.transitions.create('All', {
+        easing: theme.transitions.easing.easeOut,
+        duration: 500,
+      }),
+    },
+  }),
+}))(Box);
 
 Image.defaultProps = {
   height: '100%',
   width: '100%',
 };
 
-const Boarder = styled('div')(({ theme }) => ({
-  position: 'relative',
-  top: 32,
-  left: 32,
-  height: '100%',
-  width: '100%',
-  borderRadius: '8px',
-  border: `2px solid ${theme.palette.primary.main}`,
-  transform: 'translateZ(-16px)',
-}));
+const Boarder = styled('div')(({ theme }) => ({}));
 
 export default (props) => {
   return (
-    <Image {...props}>
-      {/* <Color hide={hover} />
-      <Saturation hide={hover} /> */}
+    <ImageBox {...props}>
       <Boarder />
-    </Image>
+    </ImageBox>
   );
 };
